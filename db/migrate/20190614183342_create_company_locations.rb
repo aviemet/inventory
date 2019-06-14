a74a1469b82c4c45510e94c8a7bc22c6ef5ac52a
Avram Walden <aviemet@gmail.com>
2019-06-14 11:34:00 -0700

    Creates Company Location join table

---
+++ db/migrate/20190614183342_create_company_locations.rb
@@ -0,0 +1,10 @@
+class CreateCompanyLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :company_locations do |t|
+      t.references :company, foreign_key: true
+      t.references :location, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
