a8d5790293859ea7d3251b3390ee763c56ec27c0
Avram Walden <aviemet@gmail.com>
2019-06-22 18:33:55 -0700

    Login view

---
+++ client/common/Auth/Login.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Button, TextInput } from 'react-native';
+
+const Login: React.FC = () => {
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	const loginUser = () => {
+
+	}
+
+	return(
+		<>
+			<TextInput
+				placeholder="Email"
+				onChangeText={text => setEmail(text)}
+				value={email}
+			/>
+			<TextInput
+				placeholder="Password"
+				secureTextEntry={true}
+				onChangeText={text => setPassword(text)}
+				value={password}
+			/>
+			<Button
+				title="Login"
+				onPress={loginUser}
+				accessibilityLabel="Login User"
+			/>
+		</>
+	);
+}
+
+export default Login;
