4cba05f1434fc023091a73bdb2a84f9f283de686
Avram Walden <aviemet@gmail.com>
2019-12-15 11:29:27 -0800

    Centers Loading icon

--- client/web/app/Components/Loading.tsx
+++ client/web/app/Components/Loading.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress';
+import styled from 'styled-components';
 
 const Loading = () => {
 	return (
-		<div>
+		<LoadingWrapper>
 			<CircularProgress />
-		</div>
+		</LoadingWrapper>
 	);
 };
+
+const LoadingWrapper = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
 
 export default Loading;
