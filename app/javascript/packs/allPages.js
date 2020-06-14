c430032e82c1afbde4d3ed341b2bccf2e2880487
Avram Walden <aviemet@gmail.com>
2020-06-14 16:31:51 -0700

    Styles select dropdowns

--- app/javascript/packs/allPages.js
+++ app/javascript/packs/allPages.js
@@ -4,3 +4,8 @@ const menuIcon = document.getElementById('sidebar-menu-toggle')
 menuIcon.onclick = e => {
   layout.classList.toggle('side-bar-closed')
 }
+
+selects = document.getElementsByTagName('select')
+for(let select of selects) {
+  select.parentElement.classList.add('select')
+}
