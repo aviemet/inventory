4be068a8f4ef049db52fc99fec5dfafe8e8ef030
Avram Walden <avram@thebatterysf.com>
2020-06-29 14:13:07 -0700

    Removes JS injection of .select css class in favor explicit use in div element

--- app/javascript/controllers/layout_controller.js
+++ app/javascript/controllers/layout_controller.js
@@ -7,14 +7,6 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ["sidebar"]
 
-  initialize() {
-    // Add the 'select' class to all select boxes so the down arrow appears
-    const selects = document.getElementsByTagName('select')
-    for(let select of selects) {
-      select.parentElement.classList.add('select')
-    }
-  }
-
   toggleSidebar() {
     this.sidebarTarget.classList.toggle("side-bar-closed")
   }
