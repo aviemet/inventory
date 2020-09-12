8b2048b07d5fa512cf82a95c3ac020d8f76dd53c
Avram Walden <aviemet@gmail.com>
2020-09-12 13:59:26 -0700

    Adds validation for Network

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,12 @@
 class Network < ApplicationRecord
   include Ownable
+
+  validates :ip, presence: true
+  validate :ip_is_not_a_host
+
+  def ip_is_not_a_host
+    if ip.prefix == 32
+      errors.add(:ip, "Must be a network, not a host")
+    end
+  end
 end
