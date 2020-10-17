94af9b958ecc6ee514c9bf07777389e07d6943c8
Avram Walden <aviemet@gmail.com>
2020-10-17 10:57:46 -0700

    Adds default active=true for person model

--- db/migrate/20200516215921_create_people.rb
+++ db/migrate/20200516215921_create_people.rb
@@ -4,7 +4,7 @@ class CreatePeople < ActiveRecord::Migration[6.0]
       t.string :first_name
       t.string :middle_name
       t.string :last_name
-      t.boolean :active
+      t.boolean :active, default: true
       t.string :employee_number
       t.string :title
       t.references :manager, null: true, foreign_key: { to_table: :people }
