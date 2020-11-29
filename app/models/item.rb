b3cf3bc075d4bca229f4d62bd6002edd716c252f
Avram Walden <aviemet@gmail.com>
2020-11-28 22:08:41 -0800

    Implements substring search on long strings

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,6 +19,20 @@ class Item < ApplicationRecord
   scope :filter_by_category, ->(category) { where item_category: category }
   scope :filter_by_model, ->(model) { where model: model }
 
+  # against: [:title, :asset_tag, :serial, :cost, :requestable, :notes],
+  # associated_against: {
+  #   model: [:name, :model_number, :notes],
+  #   item_category: [:name],
+  #   vendor: [:name, :url],
+  #   nics: [:mac],
+  #   ips: [:address]
+  # }
+
+  searchable do
+    text :title, :asset_tag, :serial, :notes
+    double :cost
+  end
+
   def self.dropdown_display
     "title"
   end
