e47f72911cabf2fc3aa524787fcd3686b1ecc56e
Avram Walden <aviemet@gmail.com>
2021-10-07 09:20:35 -0700

    Updates show pages for assignables

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,7 +7,7 @@ class Assignment < ApplicationRecord
   belongs_to :assign_toable, polymorphic: true
   belongs_to :created_by, class_name: "User", required: false
 
-  ASSIGNABLE_TYPES = %w(Item License Accessory Consumable).freeze
+  ASSIGNABLE_TYPES = %w(Item License Accessory Consumable Component).freeze
   ASSIGN_TOABLE_TYPES = %w(Person Item Location).freeze
   validates :assignable_type, inclusion: { in: ASSIGNABLE_TYPES }
   validates :assign_toable_type, inclusion: { in: ASSIGN_TOABLE_TYPES }
