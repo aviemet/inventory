03a72660db2ff9d8cf0658c2eacd2c76f8f8f479
Avram Walden <aviemet@gmail.com>
2022-05-24 17:23:29 -0700

    Fixes authenticity token bug and eliminates useless devise sessions flash notices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: -> { request.inertia? }
+
   add_flash_types :info, :error
 
   before_action :decode_id
