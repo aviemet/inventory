185ac8ea49ed3b251d49c8e2fdf9ba2a42cfdafb
Avram Walden <aviemet@gmail.com>
2022-06-21 11:27:04 -0700

    Makes navbar styles easier to change

--- app/frontend/Layouts/AppLayout/Sidebar/useNavigationStyles.ts
+++ app/frontend/Layouts/AppLayout/Sidebar/useNavigationStyles.ts
@@ -1,77 +1,92 @@
 import { createStyles } from '@mantine/core'
 
-export default createStyles(theme => ({
-	root: {
-		transition: 'width 100ms ease-in-out, min-width 100ms ease-in-out',
+export default createStyles(theme => {
+	const navbarWidth = theme.other.navbar.width
+	const iconWidth = 48
+	const borderWidth = 2
+	const navItemWidth = navbarWidth.open - iconWidth - borderWidth
 
-		'&.closed': {
-			'span, ul ul': {
-				display: 'none',
+	return {
+		root: {
+			transition: 'width 100ms ease-in-out, min-width 100ms ease-in-out',
+
+			'&.closed': {
+				'.links > ul > li > a > span': {
+					width: `calc(100% + ${iconWidth}px)`,
+				},
+
+				'span, ul ul': {
+					display: 'none',
+				},
+
+				'ul > li > ul': {
+					top: '100%',
+					left: iconWidth,
+
+					'&.up': {
+						top: 'unset',
+						bottom: '100%',
+					},
+				},
+			},
+
+			'.links > ul > li:hover': {
+				width: navItemWidth,
+
+				'& > ul': {
+					display: 'block',
+					boxShadow: theme.shadows.xs,
+				}
+			},
+
+			'ul li': {
+				position: 'relative',
+				borderLeftWidth: `${borderWidth}px`,
+				borderStyle: 'solid',
+				borderLeftColor: 'transparent',
+
+				'&.active': {
+					borderLeftColor: theme.colors[theme.primaryColor][theme.primaryShade.dark],
+				},
+
+				'&:hover': {
+					background: theme.other.colorSchemeOption(theme.colors.gray[1], theme.colors.dark[6]),
+					borderLeftColor: theme.primaryColor,
+
+					'span, & ul': {
+						display: 'flex',
+						background: theme.other.colorSchemeOption(theme.colors.gray[1], theme.colors.dark[6]),
+					},
+				},
 			},
 
 			'ul > li > ul': {
-				top: '100%',
-				left: 48,
+				position: 'absolute',
+				display: 'none',
+				flexDirection: 'column',
+				width: `calc(100% + ${iconWidth}px)`,
+				left: `calc(100% + ${iconWidth}px)`,
+				top: 0,
 
 				'&.up': {
 					top: 'unset',
-					bottom: '100%',
+					bottom: 0,
 				},
-			},
-		},
 
-		'.links > ul > li:hover': {
-			width: 185,
-
-			'& > ul': {
-				display: 'block',
-				boxShadow: theme.shadows.xs,
-			}
-		},
-
-		'ul li': {
-			position: 'relative',
-			borderLeft: '2px solid transparent',
-
-			'&:hover': {
-				background: theme.other.colorSchemeOption(theme.colors.gray[1], theme.colors.dark[6]),
-				borderLeft: `2px solid ${theme.primaryColor}`,
-
-				'span, & ul': {
-					display: 'flex',
-					background: theme.other.colorSchemeOption(theme.colors.gray[1], theme.colors.dark[6]),
-				},
-			},
-		},
-
-		'ul > li > ul': {
-			display: 'none',
-			flexDirection: 'column',
-			width: '100%',
-			left: 'calc(100% + 48px)',
-			top: 0,
-
-			'&.up': {
-				top: 'unset',
-				bottom: 0,
+				span: {
+					width: `calc(100% - ${iconWidth}px)`,
+				}
 			},
 
 			span: {
-				width: 'calc(100% - 48px)',
-			}
-		},
-
-		'span, ul > li > ul': {
-			position: 'absolute',
-			width: '100%',
-		},
-
-		span: {
-			display: 'flex',
-			height: '100%',
-			alignItems: 'center',
-			top: 0,
-			left: 48,
-		},
+				position: 'absolute',
+				top: 0,
+				left: iconWidth,
+				width: '100%',
+				display: 'flex',
+				height: '100%',
+				alignItems: 'center',
+			},
+		}
 	}
-}))
+})
