afa180faac3a85562d934b5e4e1e19846d5a3230
Avram Walden <aviemet@gmail.com>
2022-06-24 15:45:24 -0700

    Begins work on quick access menu

--- app/frontend/Components/Button/CheckinButton.tsx
+++ app/frontend/Components/Button/CheckinButton.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from '@/Components'
 import { CheckinIcon } from '@/Components/Icons'
+import { Tooltip } from '@mantine/core'
 
 const CheckinButton = ({ href }: {href: string}) => {
 	return (
-		<Link as="button" compact href={ href }><CheckinIcon /></Link>
+		<Tooltip withArrow label="Check In" position="left" transition="fade">
+			<Link as="button" compact href={ href }><CheckinIcon /></Link>
+		</Tooltip>
 	)
 }
 
--- app/frontend/Components/Button/index.tsx
+++ app/frontend/Components/Button/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Button, type ButtonProps } from '@mantine/core'
 
-const ButtonComponent = ({ children, type = 'button', ...props }: ButtonProps<'button'>) => {
+const ButtonComponent = ({ children, ...props }: ButtonProps<'button'>) => {
 	return (
-		<Button type={ type } { ...props }>{ children }</Button>
+		<Button { ...props }>{ children }</Button>
 	)
 }
 
-export default ButtonComponent
+export default Button
 
 export { default as EditButton } from './EditButton'
 export { default as DeleteButton } from './DeleteButton'
 export { default as CheckinButton } from './CheckinButton'
 export { default as CheckoutButton } from './CheckoutButton'
 export { default as ToggleColorSchemeButton } from './ToggleColorSchemeButton'
-
