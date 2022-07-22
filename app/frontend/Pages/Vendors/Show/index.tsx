f91efcd8ccc7e7afb401f5ba66ef0a68ccfc2c93
Avram Walden <aviemet@gmail.com>
2022-07-21 17:08:10 -0700

    Displays associated data tables

--- app/frontend/Pages/Vendors/Show/index.tsx
+++ app/frontend/Pages/Vendors/Show/index.tsx
@@ -4,6 +4,7 @@ import { Section, Link, Menu, Flex, Heading, Tabs, Table } from '@/Components'
 import { Routes } from '@/lib'
 import { NewIcon, EditIcon } from '@/Components/Icons'
 import ItemsTable from '@/Pages/Items/Table'
+import AccessoriesTable from '@/Pages/Accessories/Table'
 
 type ShowPageVendor = Schema.Vendor & {
 	items_count: number
@@ -11,18 +12,21 @@ type ShowPageVendor = Schema.Vendor & {
 	consumables_count: number
 	components_count: number
 	licenses_count: number
+	contracts_count: number
 }
 
 interface IVendorShowProps {
 	vendor: ShowPageVendor
+	items?: Schema.Item[]
+	accessories?: Schema.Accessory[]
+	components?: Schema.Component[]
+	consumables?: Schema.Consumable[]
+	licenses?: Schema.License[]
+	contracts?: Schema.Contract[]
 }
 
-const Show = ({ vendor }: IVendorShowProps) => {
+const Show = ({ vendor, items, accessories, components, consumables, licenses, contracts }: IVendorShowProps) => {
 	const title = vendor.name ?? 'Vendor Details'
-
-	const handleTabChange = (i: number, key: string) => {
-		console.log({ i, key })
-	}
 
 	const pagination = {}
 
@@ -46,47 +50,130 @@ const Show = ({ vendor }: IVendorShowProps) => {
 					</Menu>
 				</Flex>
 
-				<Tabs onTabChange={ handleTabChange }>
+				<Tabs useUrlHash={ true }>
+					{ /*********** ITEMS ***********/ }
 					<Tabs.Tab tabKey="items" label={ `Hardware (${vendor.items_count})` }>
 
-						<Table.Section>
+						<div className='fullHeight'>
 							<Table.TableProvider
 								selectable
 								hideable
 								model="items"
-								rows={ vendor.items }
+								rows={ items }
 								pagination={ pagination }
 							>
-
 								<Table.Title
 									title={ title }
 									menuOptions={ [
 										{ label: 'New Asset', href: Routes.newItem(), icon: NewIcon },
 									] }
 								/>
-
-								<ItemsTable items={ vendor.items ?? [] } pagination={ pagination } />
-
+								<ItemsTable />
 								<Table.Pagination />
 							</Table.TableProvider>
-						</Table.Section>
+						</div>
 					</Tabs.Tab>
 
+					{ /*********** ACCESSORIES ***********/ }
 					<Tabs.Tab tabKey="accessories" label={ `Accessories (${vendor.accessories_count})` }>
-						<Table></Table>
+						<Table.TableProvider
+							selectable
+							hideable
+							model="accessories"
+							rows={ accessories }
+							pagination={ pagination }
+						>
+							<Table.Title
+								title={ title }
+								menuOptions={ [
+									{ label: 'New Accessory', href: Routes.newAccessory(), icon: NewIcon },
+								] }
+							/>
+							<AccessoriesTable />
+							<Table.Pagination />
+						</Table.TableProvider>
 					</Tabs.Tab>
 
+					{ /*********** CONSUMABLES ***********/ }
 					<Tabs.Tab tabKey="consumables" label={ `Consumables (${vendor.consumables_count})` }>
-						<Table></Table>
+						<Table.TableProvider
+							selectable
+							hideable
+							model="consumables"
+							rows={ consumables }
+							pagination={ pagination }
+						>
+							<Table.Title
+								title={ title }
+								menuOptions={ [
+									{ label: 'New Consumable', href: Routes.newConsumable(), icon: NewIcon },
+								] }
+							/>
+							<AccessoriesTable />
+							<Table.Pagination />
+						</Table.TableProvider>
 					</Tabs.Tab>
 
+					{ /*********** COMPONENTS ***********/ }
 					<Tabs.Tab tabKey="components" label={ `Components (${vendor.components_count})` }>
-						<Table></Table>
+						<Table.TableProvider
+							selectable
+							hideable
+							model="components"
+							rows={ components }
+							pagination={ pagination }
+						>
+							<Table.Title
+								title={ title }
+								menuOptions={ [
+									{ label: 'New Component', href: Routes.newComponent(), icon: NewIcon },
+								] }
+							/>
+							<AccessoriesTable />
+							<Table.Pagination />
+						</Table.TableProvider>
 					</Tabs.Tab>
 
+					{ /*********** LICENSES ***********/ }
 					<Tabs.Tab tabKey="licenses" label={ `Licenses (${vendor.licenses_count})` }>
-						<Table></Table>
+						<Table.TableProvider
+							selectable
+							hideable
+							model="licenses"
+							rows={ licenses }
+							pagination={ pagination }
+						>
+							<Table.Title
+								title={ title }
+								menuOptions={ [
+									{ label: 'New License', href: Routes.newLicense(), icon: NewIcon },
+								] }
+							/>
+							<AccessoriesTable />
+							<Table.Pagination />
+						</Table.TableProvider>
 					</Tabs.Tab>
+
+					{ /*********** CONTRACTS ***********/ }
+					<Tabs.Tab tabKey="contracts" label={ `Contracts (${vendor.contracts_count})` }>
+						<Table.TableProvider
+							selectable
+							hideable
+							model="contracts"
+							rows={ contracts }
+							pagination={ pagination }
+						>
+							<Table.Title
+								title={ title }
+								menuOptions={ [
+									{ label: 'New Contract', href: Routes.newContract(), icon: NewIcon },
+								] }
+							/>
+							<AccessoriesTable />
+							<Table.Pagination />
+						</Table.TableProvider>
+					</Tabs.Tab>
+
 				</Tabs>
 
 			</Section>
