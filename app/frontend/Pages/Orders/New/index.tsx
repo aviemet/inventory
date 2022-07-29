bfb622128ff74e709fca7eebaf32498170c01ad6
Avram Walden <aviemet@gmail.com>
2022-07-29 08:26:12 -0700

    Creates Orders form

--- app/frontend/Pages/Orders/New/index.tsx
+++ app/frontend/Pages/Orders/New/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Head, usePage } from '@inertiajs/inertia-react'
+import { Heading, Section } from '@/Components'
+import OrderForm from '../Form'
+import { Routes } from '@/lib'
 
-const New = () => {
+interface INewOrderProps {
+	order: Schema.Order
+	vendors: Schema.Vendor[]
+}
+
+const NewOrder = ({ ...data }: INewOrderProps) => {
+	const title = 'New Purchase Order'
+
+	const { props: { auth: { user } } } = usePage<InertiaPage>()
+
 	return (
-		<div>New</div>
+		<>
+			<Head title={ title }></Head>
+
+			<Section>
+				<Heading>{ title } for { user.active_company!.name }</Heading>
+
+				<OrderForm to={ Routes.items() } { ...data } />
+			</Section>
+		</>
 	)
 }
 
-export default New
+export default NewOrder
