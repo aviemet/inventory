68a103e2713d2ea468a337ead58612f73312fc6b
Avram Walden <aviemet@gmail.com>
2022-08-03 12:38:37 -0700

    Removes console logs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,20 +16,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.save
     yield resource if block_given?
     if resource.persisted?
-      ap "PERSISTED"
+      # ap "PERSISTED"
       if resource.active_for_authentication?
-        ap "ACTIVE FOR AUTHENTICATION"
+        # ap "ACTIVE FOR AUTHENTICATION"
         # set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
       else
-        ap "Not ACTIVE FOR AUTHENTICATION"
+        # ap "Not ACTIVE FOR AUTHENTICATION"
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
         redirect_to after_inactive_sign_up_path_for(resource)
       end
     else
-      ap "Not PERSISTED"
+      # ap "Not PERSISTED"
       clean_up_passwords resource
       set_minimum_password_length
       redirect_to new_user_registration_path
