07e264b6cd6c0a387df1eb9a8ee1ff2791751266
Avram Walden <aviemet@gmail.com>
2022-08-04 14:44:54 -0700

    Sets mailer action to trustifi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,15 @@ Rails.application.configure do
   config.active_job.queue_adapter     = :sidekiq
   config.active_job.queue_name_prefix = "inventory_production"
 
+  config.action_mailer.delivery_method = :trustifi_mail
+  
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => "www.aviemet.com" }
+  config.action_mailer.default_url_options = { :host => "it-assets-inventory.herokuapp.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
