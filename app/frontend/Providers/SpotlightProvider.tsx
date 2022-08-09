b89849f9e6aa49785f6a91a4270ca3265ae329ae
Avram Walden <aviemet@gmail.com>
2022-08-09 10:55:59 -0700

    Adds spotlight

---
+++ app/frontend/Providers/SpotlightProvider.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { SpotlightProvider as MantineSpotlightProvider, openSpotlight } from '@mantine/spotlight'
+import type { SpotlightAction } from '@mantine/spotlight'
+import { SearchIcon, DashboardIcon, ItemsIcon, SettingsIcon } from '@/Components/Icons'
+import { Inertia } from '@inertiajs/inertia'
+import { Routes } from '@/lib'
+
+const actions: SpotlightAction[] = [
+	{
+		title: 'Dashboard',
+		description: 'Get full information about current system status',
+		onTrigger: () => Inertia.get(Routes.root()),
+		icon: <DashboardIcon size={ 18 } />,
+	},
+	{
+		title: 'Hardware',
+		description: 'View hardware items',
+		onTrigger: () => Inertia.get(Routes.items()),
+		icon: <ItemsIcon size={ 18 } />,
+	},
+	{
+		title: 'Settings',
+		description: 'Visit documentation to lean more about all features',
+		onTrigger: () => Inertia.get(Routes.settings()),
+		icon: <SettingsIcon size={ 18 } />,
+	},
+]
+
+const SpotlightProvider = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<MantineSpotlightProvider
+			actions={ actions }
+			searchIcon={ <SearchIcon size={ 18 } /> }
+			searchPlaceholder="Search..."
+			shortcut="mod + k"
+			nothingFoundMessage="Nothing found..."
+		>
+			{ children }
+		</MantineSpotlightProvider>
+	)
+}
+
+export default SpotlightProvider
