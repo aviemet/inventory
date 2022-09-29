4cb02a1f43539fc347a78fa36a30dd8b900609aa
Avram Walden <aviemet@gmail.com>
2022-09-29 08:55:41 -0700

    Begins work on orders form

--- app/frontend/Pages/Orders/Form/index.tsx
+++ app/frontend/Pages/Orders/Form/index.tsx
@@ -12,6 +12,9 @@ import {
 import { Routes } from '@/lib'
 import VendorsForm from '@/Pages/Vendors/Form'
 import ModelsForm from '@/Pages/Models/Form'
+import { VendorsDropdown } from '@/Components/Form/Dropdowns'
+import { Menu } from '@/Components'
+import { PlusCircleIcon } from '@/Components/Icons'
 
 export interface IOrderFormProps {
 	to: string
@@ -30,14 +33,17 @@ const OrderForm = ({ to, method = 'post', onSubmit, order, vendors }: IOrderForm
 			method={ method }
 			onSubmit={ onSubmit }
 		>
-
-			<Input name="name" label="Name" required autoFocus />
-
-			<SearchableDropdown
-				name="vendors"
-				label="Vendor"
-				options={ vendors }
+			<VendorsDropdown
+				vendors={ vendors }
 			/>
+
+			<Menu position="bottom-end">
+				<Menu.Target color="primary" variant="filled" icon={ <PlusCircleIcon /> } />
+				<Menu.Dropdown>
+					<Menu.Item>Thing</Menu.Item>
+				</Menu.Dropdown>
+			</Menu>
+
 
 			<Textarea name="notes" label="Notes" />
 
