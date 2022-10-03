d0b593f79502fb740085b64d8ced136d4c0385fa
Avram Walden <aviemet@gmail.com>
2022-10-03 12:55:45 -0700

    Replaces lifecycle callback with attribute method to set defaults

--- app/models/ldap.rb
+++ app/models/ldap.rb
@@ -2,18 +2,12 @@ class Ldap < ApplicationRecord
   resourcify
   audited
 
-  after_initialize :defaults
+  attribute :host, default: '127.0.0.1'
+  attribute :port, default: 389
 
   validates_presence_of :name
   validates_presence_of :host
   validates_presence_of :port
 
   belongs_to :company
-
-  private
-
-  def defaults
-    self.host = '127.0.0.1' unless self.host
-    self.port = 389 unless self.port
-  end
 end
