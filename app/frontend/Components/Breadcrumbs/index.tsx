efbc29b75d07577aad16e2819a3ecae02289443c
Avram Walden <aviemet@gmail.com>
2022-10-12 12:15:52 -0700

    Moves breadcrumbs to footer

--- app/frontend/Components/Breadcrumbs/index.tsx
+++ app/frontend/Components/Breadcrumbs/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
 import { Breadcrumbs as MantineBreadcrumbs, type BreadcrumbsProps, Group } from '@mantine/core'
 
 const Breadcrumbs = ({ separator = '>', ...props }: BreadcrumbsProps) => {
-	return (
+	const [footerElement, setFooterElement] = useState<HTMLElement>()
+
+	useLayoutEffect(() => {
+		if(footerElement) return
+
+		const node = document.getElementById('footer-portal')
+		if(node) {
+			setFooterElement(node)
+		}
+	}, [footerElement])
+
+	if(!footerElement) return
+
+	return createPortal(
 		<Group position="right" sx={ { marginBottom: 6 } }>
 			<MantineBreadcrumbs separator={ separator } { ...props } />
-		</Group>
+		</Group>,
+		footerElement
 	)
 }
 
