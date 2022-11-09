c1bd3c30e39b837d762f4ac7a786642e9ebf21b1
Avram Walden <aviemet@gmail.com>
2022-11-09 11:11:10 -0800

    Refactors HoverLink to be exported as a member of Tile

--- app/frontend/Components/Layout/index.ts
+++ app/frontend/Components/Layout/index.ts
@@ -1,3 +1,2 @@
-export { default as TableTitleSection } from './TableTitleSection'
+export { default as TableTitleSection } from './IndexPageTemplate/TableTitleSection'
 export { default as IndexPageTemplate } from './IndexPageTemplate'
-export { default as HoverLink } from './HoverLink'
