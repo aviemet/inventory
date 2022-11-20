44bcd81dae39c50d09b951190fc6cc67262c992b
Avram Walden <aviemet@gmail.com>
2022-11-19 18:01:55 -0800

    Fixes Money flexbox formatting

--- app/frontend/Components/Money/FlexMoney.tsx
+++ app/frontend/Components/Money/FlexMoney.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { Group } from '@mantine/core'
 
 interface IFlexMoneyProps {
@@ -7,19 +7,19 @@ interface IFlexMoneyProps {
 	accounting?: boolean
 }
 
+const getParts = (formatter: Intl.NumberFormat, value: number) => {
+	const parts = formatter.formatToParts(value)
+	const symbol = parts.find(part => part.type === 'currency')
+
+	const formattedValue = value === 0 ? '-' : parts.filter(part => part.type !== 'currency').map(part => part.value).join('')
+
+	return {
+		symbol: symbol?.value ?? '',
+		value: formattedValue,
+	}
+}
+
 const FlexMoney = ({ children, formatter, accounting }: IFlexMoneyProps) => {
-	const getParts = useCallback((formatter: Intl.NumberFormat, value: number) => {
-		const parts = formatter.formatToParts(value)
-		const symbol = parts.find(part => part.type === 'currency')
-
-		const formattedValue = value === 0 ? '-' : parts.filter(part => part.type !== 'currency').map(part => part.value).join('')
-
-		return {
-			symbol: symbol?.value ?? '',
-			value: formattedValue,
-		}
-	}, [children])
-
 	const { symbol, value } = getParts(formatter, children)
 
 	return (
