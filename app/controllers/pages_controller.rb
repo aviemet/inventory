7ab0ca3f3db3c9afd1baec992967f3f6a00289f6
Avram Walden <aviemet@gmail.com>
2023-01-09 12:12:55 -0800

    Resolves missed merge conflicts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,7 @@ class PagesController < ApplicationController
   def dashboard
     render inertia: "Pages/Dashboard", props: {
       company: @active_company.render(view: :counts),
-<<<<<<< HEAD
-      activities: ActivityBlueprint.render_as_json(PublicActivity::Activity.last(10), view: :dashboard)
-=======
       activities: ActivityBlueprint.render_as_json(PublicActivity::Activity.includes(:owner, :trackable, :recipient).last(10), view: :dashboard)
->>>>>>> dev
     }
   end
 
