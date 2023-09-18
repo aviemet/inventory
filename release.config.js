197b61b5c68eb8fe3c736c249f1bd44b51df2931
Avram Walden <aviemet@gmail.com>
2023-09-18 08:44:00 -0700

    Tries release.config.js file

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	release: {
+		branches: ['release'],
+	},
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/github',
+		['@semantic-release/git', {
+			'message': 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+		}],
+	],
+}
