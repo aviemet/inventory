6e62617965e9d74980af7fd1d8b68b05c41e93f1
Avram Walden <aviemet@gmail.com>
2023-09-18 09:02:38 -0700

    back to basics

--- release.config.js
+++ release.config.js
@@ -1,23 +1,7 @@
 module.exports = {
 	release: {
 		branches: [
-			'+([0-9])?(.{+([0-9]),x}).x',
 			'release',
-			'next',
-			'next-major',
-			{ name: 'beta', prerelease: true },
-			{ name: 'alpha', prerelease: true },
 		],
 	},
-	plugins: [
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
-		'@semantic-release/changelog',
-		'@semantic-release/github',
-		['@semantic-release/git', {
-			'message': 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
-		}],
-	],
-	mangle: false,
-	headerIds: false,
 }
