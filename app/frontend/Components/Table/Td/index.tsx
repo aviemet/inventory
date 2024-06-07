60bfd6f02f3e84c4b561c7a16902f591cb5f039b
Avram Walden <aviemet@gmail.com>
2024-06-07 11:50:03 -0700

    fix: removes totally unecessary nowrap prop from table cells

--- app/frontend/Components/Table/Td/index.tsx
+++ app/frontend/Components/Table/Td/index.tsx
@@ -7,7 +7,6 @@ import { usePageProps } from '@/lib/hooks'
 export interface TableCellProps extends TableTdProps {
 	fitContent?: boolean
 	sort?: string
-	nowrap?: boolean
 	hideable?: false|string
 	ref?: React.RefObject<HTMLTableCellElement>
 }
