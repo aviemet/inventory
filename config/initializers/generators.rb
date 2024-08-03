31080c2ccd24a942c37260fcf2a5284ee00667c0
Avram Walden <aviemet@gmail.com>
2024-08-03 11:11:12 -0700

    chore: adds serializer generator to scaffold generator run

---
+++ config/initializers/generators.rb
@@ -0,0 +1,11 @@
+require 'rails/generators/rails/scaffold/scaffold_generator'
+
+module Rails
+  module Generators
+    class ScaffoldGenerator
+      def add_serializer
+        invoke "serializer", [name.singularize]
+      end
+    end
+  end
+end
